5e54677a628941c60786f152054e80e73f4ba3c4
Sasoed <seva.romanovsky@gmail.com>
2025-05-17 22:28:04 +0300

    Автоматическое обновление конфигов

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -52,7 +52,7 @@ function vv
         cd ..
     end
     cd $D
-    read -r -p "$FILE not found, do you want to create venv? [y/N]" ans
+    read -l -P "$FILE not found, do you want to create venv? [y/N]" ans
     switch %ans
         case y Y
             if python3 -m venv venv; then
